8d1ed6cf295aecde1a471441c36a498a62907cc3
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-06-16 12:43:36 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
