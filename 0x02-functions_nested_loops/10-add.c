09b6bb43aa5c9e6fbba528e7e61be693a1c78fe4
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-06-20 13:23:39 -0700

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-/**
- * add - Check Main
- * @a: An integer a
- * @b: An integer b
- * Description: Function that adds two integers and returns the result
- * Return: Result to add a and b
- */
+int add(int a, int b); // Function declaration for add
+
+int main(void)
+{
+    int n = add(89, 9);
+    // Rest of your code
+
+    return 0;
+}
+
 int add(int a, int b)
 {
-	return (a + b);
+    return (a + b);
 }
