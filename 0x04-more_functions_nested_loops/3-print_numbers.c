15be01cd52de93e03207cc36c4fbeab82fa9efe1
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-06-22 10:36:23 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Prints numbers between 0 to 9.
+ * No return.
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 18; ch < 28; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
