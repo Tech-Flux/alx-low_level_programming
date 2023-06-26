6fcf383f7a06d6891b53469d0edeefcd8b14e289
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-06-26 12:54:07 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
