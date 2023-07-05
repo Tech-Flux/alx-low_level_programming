a83a144dfdfa8ee28269eaaadd496b5b32a0e0e3
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-07-05 13:31:22 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion:  prints a string in reverse
+ * _print_rev_recursion:  prints  string in reverse
  * main: checks the code
  * return: always ret 0
  * @s: string
@@ -10,6 +10,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-                _putchar(*s);
+		_putchar(*s);
 	}
 }
